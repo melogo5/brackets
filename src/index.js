f0c58c0cebd9f0907a87ab77299f3e53891faf97
melogo5 <i.plyton@gmail.com>
2021-02-23 23:49:28 +0500

    feat: add check() implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(string, bracketsConfig) {
+	let flag = false;
+
+	while(string != '') {
+	flag = false;
+		for(let conf of bracketsConfig) {
+			const brackets = conf.join('');
+			if(string.includes(brackets)) {
+				string = string.replace(brackets, '');
+				flag = true;
+			}
+		}
+		if(!flag) break;
+	}
+
+	if (string == '') {return true} 
+	return false;
 }
